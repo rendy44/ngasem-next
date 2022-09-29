c9020f39d00dfd9bb326168f74ecffa87e0cba15
Rendy <rendy.de.p@gmail.com>
2022-09-29 12:00:00 +0700

    Adding student detail page

--- pages/student/[id].js
+++ pages/student/[id].js
@@ -1,8 +1,10 @@
-import {Footer, Loader, Section, TopNav} from "../../components/global";
+import {Footer, Loader, Section} from "../../components/global";
 import {dataService} from "../../services/data.service";
 import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import Styles from '../../styles/pages/student.module.scss'
+import logoSmk from '../../public/graduating-student.png'
+import Image from "next/image";
 
 const StudentDetailPage = () => {
     const router = useRouter();
@@ -24,11 +26,73 @@ const StudentDetailPage = () => {
             }
         }
     })
-    const studentContent = isLoaded ? <></> : <Loader/>
+    const studentContent = isLoaded ? <div className={Styles.detail_wrapper}>
+        <div className={'frow'}>
+            <div className={'col-md-2-3'}>
+                <div className={Styles.hero}>
+                    <div className={Styles.cover}>
+                        <span>Photo by iam_os on Unsplash</span>
+                    </div>
+                    <div className={Styles.profile}>
+                        <div className={Styles.profile_image}>
+                            <Image src={logoSmk} className={Styles.img}/>
+                        </div>
+                        <div className={Styles.profile_info}>
+                            <h1 className={Styles.name}>{data.name}</h1>
+                            <p className={Styles.grade}>{data.grade}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.detail}>
+                    <h2>Informasi</h2>
+                    <div className={Styles.details_wrapper}>
+                        <div className={Styles.detail_item}>
+                            <label htmlFor="name">Nama lengkap</label>
+                            <span id="name">{data.name}</span>
+                        </div>
+                        <div className={Styles.detail_item}>
+                            <label htmlFor="nis">NIS</label>
+                            <span id="nis">{data.nis}</span>
+                        </div>
+                        <div className={Styles.detail_item}>
+                            <label htmlFor="grade">Kelas</label>
+                            <span id="grade">{data.grade}</span>
+                        </div>
+                        {parseInt(data.point) > 0 ? <div className={Styles.detail_item}>
+                            <label htmlFor="point">Poin terkumpul</label>
+                            <span id="point">{data.point} poin</span>
+                        </div> : <></>}
+                    </div>
+                </div>
+                <div className={Styles.penalties}>
+                    <h2>Pelanggaran</h2>
+                    <div className={Styles.penalties_wrapper}>
+                        {data.penalties ?
+                            Object.entries(data.penalties).map((penalty, i) => {
+                                return <div className={Styles.detail_item} key={i}>
+                                    <div className={Styles.item_top}>
+                                        <span>Oleh <strong>{penalty[1].teacher}</strong> pada {penalty[1].date} sebesar {penalty[1].point} poin</span>
+                                    </div>
+                                    {penalty[1].description ?
+                                        <div className={Styles.item_body}>
+                                            <blockquote><p>{penalty[1].description}</p></blockquote>
+                                        </div> : <></>}
+                                    <div className={Styles.item_bottom}>
+                                        <ul>
+                                            <li>{penalty[1].penalty}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            })
+                            : <p className={Styles.empty}>Belum ada pelanggaran yang tercatat</p>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> : <Loader/>
     return (
         <>
-            <TopNav/>
-            <Section id={'studentDetail'} extraClass={Styles.student}>
+            <Section id={'studentDetail'} extraClass={isLoaded ? `${Styles.student} ${Styles.loaded}` : Styles.student}>
                 {studentContent}
             </Section>
             <Footer/>
